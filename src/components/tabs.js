dd120536c7f96008334e09ea57a583691e7e6730
Owen McComas <owen@mccomas.me>
2022-12-01 16:46:53 -0700

    tabs.js complete

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,4 +1,19 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
+
+
+const topicWrap = document.createElement('div');
+
+topicWrap.classList.add('topics');
+
+for (let i = 0; i < topics.length; i++) {
+  const tabEl = document.createElement('div');
+  tabEl.classList.add('tab');
+  tabEl.textContent = topics[i];
+  topicWrap.appendChild(tabEl)
+};
+
   // TASK 3
   // ---------------------
   // Implement this function which takes an array of strings ("topics") as its only argument.
@@ -13,9 +28,16 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
+  return topicWrap;
 }
 
 const tabsAppender = (selector) => {
+
+  axios.get('http://localhost:5001/api/topics')
+    .then(resp => {
+      document.querySelector(selector).appendChild(Tabs(resp.data.topics))
+    }) .catch(err => console.error(err))
+
   // TASK 4
   // ---------------------
   // Implement this function which takes a css selector as its only argument.
